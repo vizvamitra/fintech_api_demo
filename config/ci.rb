47201ea688a47bfec1f9b6f35b58cebe3eabb112
vizvamitra <vizvamitra@users.noreply.github.com>
2026-07-06 05:02:44 +0400

    add rspec step to CI

--- config/ci.rb
+++ config/ci.rb
@@ -4,6 +4,7 @@ CI.run do
   step "Setup", "bin/setup --skip-server"
 
   step "Style: Ruby", "bin/rubocop"
+  step "Tests: RSpec", "bundle exec rspec"
 
   step "Security: Gem audit", "bin/bundler-audit"
   step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
